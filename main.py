2d547b9b47cbb882ca859ac5f508f772890d61eb
faoflynn <charliebc96@gmail.com>
2025-07-27 15:11:44 -0400

    added sort list

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_letters, sort_letters
 def get_book_text(filepath):
     filecontent = ""
     with open(filepath) as f:
@@ -6,8 +6,11 @@ def get_book_text(filepath):
     return filecontent
 
 def main():
-    book = get_book_text("./books/frankenstein.txt")
+    book_url = "books/frankenstein.txt"
+    book = get_book_text(book_url)
     num_words = count_words(book)
-    print(f"{num_words} words found in the document")
+    #print(f"{num_words} words found in the document")
+    letter_count = count_letters(book)
+    #print(letter_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,22 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_letters(text):
+    character_list = list(text.lower())
+    character_set = set(character_list)
+    character_count = {}
+    for character in character_set:
+        character_count[character] = 0
+    for character in character_list:
+        character_count[character] += 1
+    return character_count
+
+def sort_on(items):
+    return items["num"]
+
+def sort_letters(dict):
+    list_of_letters = []
+    for character, count in dict:
+        list_of_letters.append({"char": character, "num": count})
+    list_of_letters.sort(reverse=True, key=sort_on)
